4d3e24df54a12b83a79df1ff30d7d06cafedb3b3
Gerrard Ezeugwa <ebubeezeugwa@gmail.com>
2023-06-01 16:44:24 +0100

    commit

--- src/components/top-menu/top-menu.component.jsx
+++ src/components/top-menu/top-menu.component.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { TopMenuContainer } from "./top-menu.styles";
 import TopMenuDesktop from "../top-menu-desktop/top-menu-desktop.component";
 import TopMenuMobile from "../top-menu-mobile/top-menu-mobile.component";
